17083f5e0aa835442cd1023a364b0ae14c60c378
sekortest <36673250+sekortest@users.noreply.github.com>
2018-03-05 15:18:09 +0200

    Description added

--- replace.sh
+++ replace.sh
@@ -1,1 +1,5 @@
+# > echo "ntcnHjprkflrb" | ./replace.sh
+# > тестРозкладки
+
+
 sed  -E "s/Q/й/g;s/w/ц/g;s/e/у/g;s/r/к/g; s/t/е/g; s/y/н/g; s/u/г/g; s/i/ш/g; s/o/щ/g; s/p/з/g; s/a/ф/g; s/s/і/g; s/d/в/g; s/f/а/g; s/g/п/g; s/h/р/g; s/j/о/g; s/k/л/g; s/l/д/g ; s/z/я/g; s/x/ч/g ; s/c/с/g; s/v/м/g; s/b/и/g; s/n/т/g;s/m/ь/g;s/\[/х/g;s/]/ї/g; s/;/ж/g;  s/,/б/g; s/\./ю/g; s/Q/Й/g; s/W/Ц/g ; s/E/У/g; s/R/К/g; s/T/Е/g; s/Y/Н/g; s/U/Г/g; s/I/Ш/g   ; s/O/Щ/g; s/P/З/g; s/A/Ф/g; s/S/І/g; s/D/В/g ; s/F/А/g; s/G/П/g ; s/H/Р/g; s/J/О/g;s/K/Л/g;s/L/Д/g;s/Z/Я/g;s/X/Ч/g; s/C/С/g; s/V/М/g; s/B/И/g; s/N/Т/g; s/M/Ь/g ; s/\{/Х/g; s/}/Ї/g; s/:/Ж/g; s/</Б/g; s/\>/Ю/g "
